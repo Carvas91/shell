2e2771d0c169197b6e6d34477feb2af0fdb38579
carvas91 <carvas91@hotmail.com>
2024-11-30 10:39:51 -0500

    Third iteration after Moniz feedback on th PR.

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -28,8 +28,8 @@ mkdir data
 
 # 2. Move the ./rawdata directory to ./data/raw
 
-mkdir ./data/raw
-mv ./rawdata/* ./data/raw
+#mkdir ./data/raw
+mv ./rawdata ./data/raw
 
 # 3. List the contents of the ./data/raw directory
 
